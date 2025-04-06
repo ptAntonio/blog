fca8fee0dc695411754ae6b7b03b196f9ea9c598
Antonio Pérez <64748202+AntPerDev@users.noreply.github.com>
2025-04-06 12:14:09 +0200

    despliegue github netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+
   vite: {
     plugins: [tailwindcss()]
   },
+  site: 'https://ptantonio.github.io',
+  base: 'blog',
+
+  adapter: netlify(),
 });
