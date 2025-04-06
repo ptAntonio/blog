2f537ddf92b9450efa73d69c91a0506d32cead26
Antonio Pérez <64748202+AntPerDev@users.noreply.github.com>
2025-04-06 11:43:31 +0200

    blog with posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,14 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: 'content',
-  schema: z.object({
+  schema: ({image}) => z.object({
     title: z.string() ,
     date: z.date() ,
     description: z.string() ,
-    image: z.string() ,
+    image: image(),
+    // .refine( (img) => img.width<1200 , {
+    //   message:'Image should be lower than 1200px',
+    // }),
 
     // Relación
     author: z.string() ,
