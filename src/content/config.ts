792f4dbc0e7809906fa4a3294f6273394901871d
Antonio Pérez <64748202+AntPerDev@users.noreply.github.com>
2025-04-09 19:19:26 +0200

    todos los post de cada autor

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: 'content',
@@ -12,13 +12,23 @@ const blogCollection = defineCollection({
     // }),
 
     // Relación
-    author: z.string() ,
+    // author: z.string() ,
+    author: reference('author'),
 
     // Relación
     tags: z.array(z.string()) ,
   }),
 })
 
+const authorCollection = defineCollection({
+  type: 'data',
+  schema:({image}) => z.object({
+    name: z.string(),
+    avatar: image()
+  })
+})
+
 export const collections = {
-  'blog': blogCollection
+  'blog': blogCollection,
+  'author': authorCollection
 }
